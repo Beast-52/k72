576723c87b77c6f2df0d6ce6d23bb052a059102c
Beast-52 <ara62580@gmail.com>
2025-09-08 23:26:06 +0500

    fet : Routing ✅✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Agence from "./Pages/Agence";
+import Contact from "./Pages/Contact";
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold ">
-      I'm still Running
-    </div>
-  )
-}
+    <>
+      <nav className="flex gap-4 text-lg font-bold text-black bg-red-400">
+        <Link className="hover:underline" to="/">
+          Home
+        </Link>{" "}
+        |{" "}
+        <Link className="hover:underline" to="/agence">
+          Agence
+        </Link>{" "}
+        |{" "}
+        <Link className="hover:underline" to="/contact">
+          Contact
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agence" element={<Agence />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
